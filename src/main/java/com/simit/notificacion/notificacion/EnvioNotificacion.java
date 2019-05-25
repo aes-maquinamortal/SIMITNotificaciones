df31a686dab20cd22bb20457773c2e407943fb91
MinMiguelM <cmiguelmg@gmail.com>
2019-05-25 13:23:14 -0500

    integration with queue

--- src/main/java/com/simit/notificacion/notificacion/EnvioNotificacion.java
+++ src/main/java/com/simit/notificacion/notificacion/EnvioNotificacion.java
@@ -1,15 +1,39 @@
 package com.simit.notificacion.notificacion;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.jws.WebMethod;
 
+import com.google.gson.Gson;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EnvioNotificacion {
 	
+	public final static String MAIL_QUEUE = "send_mail";
+	
 	@WebMethod(action = "envioNotificacionUtilidad")
 	public void envioNotificacionUtilidad(String correo, String tipo, String jsonPayload) {
-			System.out.println(jsonPayload);
+		ConnectionFactory factory = new com.rabbitmq.client.ConnectionFactory();
+		factory.setHost("localhost");
+		try ( Connection connection = factory.newConnection();
+			  Channel channel = connection.createChannel()
+			) {
+			final Map<String, String> message = new HashMap();
+			message.put("correo", correo);
+			message.put("tipo", tipo);
+			message.put("payload", jsonPayload);
+			final Gson gson = new Gson();
+			final String messageBytes = gson.toJson(message);
+			channel.queueDeclare(MAIL_QUEUE, false, false, false, null);
+			channel.basicPublish("", MAIL_QUEUE, null, messageBytes.getBytes());
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 	}
 
 }
--- src/main/java/com/simit/notificacion/notificacion/NotificacionApplication.java
+++ src/main/java/com/simit/notificacion/notificacion/NotificacionApplication.java
@@ -1,13 +1,43 @@
 package com.simit.notificacion.notificacion;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeoutException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.google.gson.Gson;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
 
 @SpringBootApplication
 public class NotificacionApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, TimeoutException {
 		SpringApplication.run(NotificacionApplication.class, args);
+		startQueueConsumer();
+	}
+	
+	@SuppressWarnings("unchecked")
+	private static void startQueueConsumer() throws IOException, TimeoutException {
+		System.out.println("Queue consumer is running...");
+		DeliverCallback callback = (consumerTag, delivery) -> {
+			String message = new String(delivery.getBody(), "UTF-8");
+			final Gson gson = new Gson();
+			final Map<String, String> messageMap = gson.fromJson(message, HashMap.class);
+		};
+		ConnectionFactory factory = new ConnectionFactory();
+	    factory.setHost("localhost");
+	    factory.setPort(5672);
+	    Connection connection = factory.newConnection();
+	    Channel channel = connection.createChannel();
+
+	    channel.queueDeclare(EnvioNotificacion.MAIL_QUEUE, false, false, false, null);
+	    channel.basicConsume(EnvioNotificacion.MAIL_QUEUE, true, callback, consumerTag -> { });
 	}
 
 }
